cdf9c7dad42997814367ceaffa021b67e15bde50
jonasbeltoft <jonasb5@hotmail.dk>
2025-03-04 21:27:18 +0100

    removed bin and obj

--- frontend/BlazorApp/obj/Debug/net9.0/BlazorApp.AssemblyInfo.cs
+++ frontend/BlazorApp/obj/Debug/net9.0/BlazorApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eb39b4e128de1cfe69a0e32ca4ed86e8232226ae")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e10fdc3bf82dcb1b0017312157d1c320f817e8f")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
